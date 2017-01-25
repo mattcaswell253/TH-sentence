29660663cfdeb1cb96c31629e4aa0eb5041420a6
Matt Caswell <matt@>
2017-01-24 16:12:29 -0800

    fix first spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
   $("form#sentence").submit(function(event) {
     event.preventDefault();
     var firstSentence = $("#firstSentence").val();
-
-    $("#result").text(firstSentence);
+    var up = firstSentence.toUpperCase();
+    $("#result").text(up);
+    // if ($(firstSentence).charAt(0) === t) 
 
   })
 })
