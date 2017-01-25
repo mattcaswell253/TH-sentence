c83c300772e0d1a80ed7f805d165309e7ce3f435
mattcaswell <matthewcaswell253@Gmail.com>
2017-01-24 20:04:51 -0800

    fix spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
   $("form#sentence").submit(function(event) {
     event.preventDefault();
     var firstSentence = $("#firstSentence").val();
-    var up = firstSentence.toUpperCase();
-    $("#result").text(up);
-    // if ($(firstSentence).charAt(0) === t) 
+    // var up = firstSentence.toUpperCase();
+    var words = firstSentence.split(" ");
+    // $("#result").text(up);
+    $("#result").text(words);
 
   })
+  function thcase()
 })
