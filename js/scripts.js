0338484d02aecaf5547179c396ee24cb5f9e58e7
Staff <epicodus_staff@staff.local>
2017-01-26 14:58:08 -0800

    setup for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
+function wordReplace(splitWord) {
+var splitSentence = firstSentence.split(" ");
+var splitWord = splitSentence[i];
+
+for (var i = 0; i < splitWord.length; i++) {
+  if (splitWord[0] === "t" && splitWord[1] === "h")
+  splitWord.toUpperCase();
+}
+
+}
+
+
+
+
 //user logic
 $(document).ready(function() {
   $("form#sentence").submit(function(event) {
     event.preventDefault();
     var firstSentence = $("#firstSentence").val();
-    var words = firstSentence.split(" ");
-    var th = words.startsWith("th");
+    var splitSentence = firstSentence.split(" ");
+    // var th = words.startsWith("th");
     // var up = firstSentence.toUpperCase();
     // $("#result").text(up);
-    $("#result").text(words);
+    $("#result").text(wordReplace);
+
+    wordReplace(splitWord)
   })
 })
-
-
-  if(th === true)
- firstSentence.toUpperCase();
