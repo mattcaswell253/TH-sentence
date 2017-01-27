6d7b763818a8e971dcc8f357e692fdc8113dc8ba
matt caswell <matt@>
2017-01-26 16:36:31 -0800

    finish todays work

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,14 @@ function wordReplace(firstSentence) {
   for (var i = 0; i < splitSentence.length; i++) {
     var splitWord = splitSentence[i];
     if (splitWord[0] === "t" && splitWord[1] === "h") {
-      return splitWord.toUpperCase();
+      //these will have to end up WITHIN answer, in the right order
+      answer = splitWord.toUpperCase();
     } else {
-      return splitWord;
+      //these will have to end up WITHIN answer, in the right order
+      answer = splitWord;
     }
   }
+  //this is going to be the complete, altered sentence
   return answer;
 }
 
@@ -19,13 +22,13 @@ function wordReplace(firstSentence) {
 $(document).ready(function() {
   $("form#sentence").submit(function(event) {
     event.preventDefault();
-    var firstSentence = $("#firstSentence").val();
-    var splitSentence = firstSentence.split(" ");
+    // var firstSentence = $("#firstSentence").val();
+    // var splitSentence = firstSentence.split(" ");
     // var th = words.startsWith("th");
     // var up = firstSentence.toUpperCase();
     // $("#result").text(up);
-    // $("#result").text(splitWord);
+     $("#result").text(wordReplace);
 
-    wordReplace(firstSentence);
+    // wordReplace(answer);
   })
 })
