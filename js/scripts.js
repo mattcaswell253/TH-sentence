65268498bc7414c076a975a6d19ec491c0006f15
mattcaswell <matthewcaswell253@Gmail.com>
2017-01-27 13:02:23 -0800

    finish my TH- assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
 function wordReplace(firstSentence) {
   var splitSentence = firstSentence.split(" ");
+  var newWord = "";
   var answer = "";
-  var firstAnswer = "";
-  var secondAnswer = "";
-
   for (var i = 0; i < splitSentence.length; i++) {
     var splitWord = splitSentence[i];
     if (splitWord[0] === "t" && splitWord[1] === "h") {
       //these will have to end up WITHIN answer, in the right order
-      firstAnswer = splitWord.toUpperCase();
+      newWord = splitWord.toUpperCase();
     } else {
       //these will have to end up WITHIN answer, in the right order
-      secondAnswer = splitWord.toLowerCase();
+      newWord = splitWord.toLowerCase();
     }
+    answer = answer + ' ' + newWord;
   }
   //this is going to be the complete, altered sentence
-  answer = firstAnswer + secondAnswer;
   return answer;
 }
 
@@ -26,12 +24,12 @@ function wordReplace(firstSentence) {
 $(document).ready(function() {
   $("form#sentence").submit(function(event) {
     event.preventDefault();
-    // var firstSentence = $("#firstSentence").val();
+    var firstSentence = $("#firstSentence").val();
     // var splitSentence = firstSentence.split(" ");
     // var th = words.startsWith("th");
     // var up = firstSentence.toUpperCase();
     // $("#result").text(up);
-     $("#result").text(wordReplace);
+     $("#result").text(wordReplace(firstSentence));
 
     // wordReplace(answer);
   })
